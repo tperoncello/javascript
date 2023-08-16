aec86f38699cbf944e3b8300491a2f3c29ba2c88
Tomas Peroncello <tomas.agustin.peroncello@gmail.com>
2023-08-15 23:11:15 -0300

    dom y local storage

--- js/script.js
+++ js/script.js
@@ -1,30 +1,87 @@
 // Objeto para almacenar el usuario registrado
 let usuarioRegistrado = null;
 
+// Cargar datos almacenados en Local Storage al cargar la página
+document.addEventListener("DOMContentLoaded", function () {
+  cargarDatosUsuario();
+});
+
+// Función para cargar datos almacenados en Local Storage
+function cargarDatosUsuario() {
+  const usuarioGuardadoJSON = localStorage.getItem("usuarioRegistrado");
+  if (usuarioGuardadoJSON) {
+    usuarioRegistrado = JSON.parse(usuarioGuardadoJSON);
+  }
+}
+
 // Función para registrar un nuevo usuario
 function registrarUsuario() {
-  let nombre = prompt("Ingrese su nombre:");
-  let apellido = prompt("Ingrese su apellido:");
-  const dni = prompt("Ingrese su DNI:");
+  const nombre = document.getElementById("firstNameInput").value;
+  const apellido = document.getElementById("lastNameInput").value;
+  const email = document.getElementById("emailInput").value;
+  const password = document.getElementById("passwordInput").value;
 
-  // Convertir la primera letra del nombre y el apellido a mayúscula
-  nombre = nombre.charAt(0).toUpperCase() + nombre.slice(1);
-  apellido = apellido.charAt(0).toUpperCase() + apellido.slice(1);
+  if (!nombre || !apellido || !email || !password) {
+    alert("Por favor, complete todos los campos.");
+    return;
+  }
+
+  if (!isValidEmail(email)) {
+    alert("Por favor, ingrese una dirección de correo electrónico válida.");
+    return;
+  }
+
+  const nombreCapitalizado = nombre.charAt(0).toUpperCase() + nombre.slice(1);
+  const apellidoCapitalizado = apellido.charAt(0).toUpperCase() + apellido.slice(1);
 
   usuarioRegistrado = {
-    nombre: nombre,
-    apellido: apellido,
-    dni: dni
+    nombre: nombreCapitalizado,
+    apellido: apellidoCapitalizado,
+    email: email,
+    password: password
   };
+
+  const usuarioRegistradoJSON = JSON.stringify(usuarioRegistrado);
+  localStorage.setItem("usuarioRegistrado", usuarioRegistradoJSON);
 
   console.log("Usuario registrado exitosamente.");
   console.log("Datos del usuario:");
   console.log("Nombre: " + usuarioRegistrado.nombre);
   console.log("Apellido: " + usuarioRegistrado.apellido);
-  console.log("DNI: " + usuarioRegistrado.dni);
+  console.log("Correo Electrónico: " + usuarioRegistrado.email);
+
+  // Redirigir a la página de préstamo después del registro exitoso
+  window.location.href = "pages/prestamo.html"; // Cambia esto por la URL correcta
 }
 
-registrarUsuario();
+// Función para validar formato de correo electrónico
+function isValidEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
+
+// Función para realizar el inicio de sesión
+function logIn() {
+  const email = document.getElementById("emailInput").value;
+  const password = document.getElementById("passwordInput").value;
+
+  const usuarioGuardadoJSON = localStorage.getItem("usuarioRegistrado");
+  if (usuarioGuardadoJSON) {
+    const usuarioGuardado = JSON.parse(usuarioGuardadoJSON);
+    if (usuarioGuardado.email === email && usuarioGuardado.password === password) {
+      console.log("Inicio de sesión exitoso.");
+      console.log("Bienvenido, " + usuarioGuardado.nombre + " " + usuarioGuardado.apellido + "!");
+      window.location.href = "pages/prestamo.html"; // Redirigir después del inicio de sesión exitoso
+    } else {
+      console.log("Correo electrónico o contraseña incorrectos.");
+      // Agregar aquí un mensaje en la página para mostrar el error al usuario si lo deseas
+    }
+  } else {
+    console.log("No se encontró ningún usuario registrado.");
+  }
+}
+
+
 
 // Array con la cantidad de cuotas disponibles
 const cuotasDisponibles = [3, 6, 9, 12, 18, 24];
